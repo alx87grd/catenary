bug in gradient of method x

catenary/catenary.py
@@ -12,7 +12,7 @@ from scipy.optimize import minimize
 import time
 
 ###########################
-# Catanery function
+# Catenary function
 ###########################
 
 ############################
@@ -45,7 +45,7 @@ def cat( x , a = 1. ):
 ############################
 def w_T_c( phi , x , y , z):
     """ 
-    Tranformation matrix from catenery local frame to world frame
+    Tranformation matrix from catenary local frame to world frame
     
     inputs
     ----------
@@ -56,7 +56,7 @@ def w_T_c( phi , x , y , z):
     
     outputs
     ----------
-    world_T_catenery  : 4x4 Transformation Matrix
+    world_T_catenary  : 4x4 Transformation Matrix
     
     """
     
@@ -95,7 +95,7 @@ def p2r_w( p , x_min = -200, x_max = 200 , n = 400, ):
     r_w[0,:] : x positions in world frame
     r_w[1,:] : y positions in world frame
     r_w[2,:] : z positions in world frame
-    x_c      : vector of x coord in catenery frame
+    x_c      : vector of x coord in catenary frame
     
     """
     
@@ -108,7 +108,7 @@ def p2r_w( p , x_min = -200, x_max = 200 , n = 400, ):
     phi = p[3]
     a   = p[4]
     
-    # catenery frame z
+    # catenary frame z
     z_c      = cat( x_c , a )
     
     r_c      = np.zeros((4,n))
@@ -174,7 +174,7 @@ def d_min_sample( p , pts , n_sample = 1000 , x_min = -200, x_max = 200 ):
     return d_min
 
 
-default_cost_param = [ 'sample' , np.diag([ 1.0 , 1.0 , 1.0 , 10.0 , 0.01 ]) ,
+default_cost_param = [ 'sample' , np.diag([ 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ]) ,
                         1.0 , 1.0 , 2 , 1000 , -200 , 200] 
 
 ############################
@@ -195,20 +195,47 @@ def J( p , pts , p_nom , param = default_cost_param ):
     x_min  = param[6]
     x_max  = param[7]
     
+    ###################################################
     if method == 'sample':
+        """ data association is sampled-based """
     
         # Minimum distance to points
         d_min = d_min_sample( p , pts , n , x_min , x_max )
-        
-        # Cost shaping function
-        c = lorentzian( d_min , l , power , b )
-        
-        # Average costs per measurement plus regulation
-        pts_cost = c.sum() / m 
-        
+    
+    ###################################################
     elif method == 'x':
+        """ data association is based on local x-coord in cat frame """
         
-        pass
+        x0  = p[0]
+        y0  = p[1]
+        z0  = p[2]
+        psi = p[3]
+        a   = p[4]
+        
+        c_T_w = np.linalg.inv( w_T_c( psi , x0 , y0 , z0 ) )
+        
+        r_w = np.vstack( [ pts , np.ones(m) ] )
+        
+        # Compute measurements points positions in local cable frame
+        r_c  = c_T_w @ r_w
+        
+        # Compute expected z position based on x coord and catanery model
+        z_hat  = cat( r_c[0,:] , a )
+        
+        # Compute delta vector between measurements and expected position
+        e      = np.zeros((3,m))
+        e[1,:] = r_c[1,:]
+        e[2,:] = r_c[2,:] - z_hat
+        
+        d_min = np.linalg.norm( e , axis = 0 )
+        
+    ###################################################
+        
+    # Cost shaping function
+    c = lorentzian( d_min , l , power , b )
+    
+    # Average costs per measurement plus regulation
+    pts_cost = c.sum() / m 
     
     # Regulation
     p_e = p_nom - p
@@ -217,6 +244,7 @@ def J( p , pts , p_nom , param = default_cost_param ):
     cost = pts_cost + p_e.T @ Q @ p_e
     
     return cost
+
 
 ############################
 def dJ_dp( p , pts , p_nom , param = default_cost_param , num = False ):
@@ -255,6 +283,12 @@ def dJ_dp( p , pts , p_nom , param = default_cost_param , num = False ):
         x_min  = param[6]
         x_max  = param[7]
         
+        x0  = p[0]
+        y0  = p[1]
+        z0  = p[2]
+        phi = p[3]
+        a   = p[4]
+        
         if method == 'sample':
         
            # generate a list of sample point on the model curve
@@ -278,10 +312,6 @@ def dJ_dp( p , pts , p_nom , param = default_cost_param , num = False ):
            # Error Grad for each pts
            eT_de_dp = np.zeros( (5 , pts.shape[1] ) )
            
-           
-           phi = p[3]
-           a   = p[4]
-           
            eT_de_dp[0,:] = -e_min[0,:]
            eT_de_dp[1,:] = -e_min[1,:]
            eT_de_dp[2,:] = -e_min[2,:]
@@ -294,7 +324,54 @@ def dJ_dp( p , pts , p_nom , param = default_cost_param , num = False ):
             
         elif method == 'x':
             
-            pass
+            # Pts in world frame
+            x_w = pts[0,:]
+            y_w = pts[1,:]
+            z_w = pts[2,:]
+            
+            # Compute measurements points positions in local cable frame
+            r_w   = np.vstack( [ pts , np.ones(m) ] )
+            c_T_w = np.linalg.inv( w_T_c( phi , x0 , y0 , z0 ) )
+            r_c   = c_T_w @ r_w
+            
+            x_c   = r_c[0,:]
+            y_c   = r_c[1,:]
+            z_c   = r_c[2,:]
+            
+            # Model z
+            z_hat = cat( x_c , a )
+            
+            # Error in local frame
+            e      = np.zeros((3,m))
+            e[1,:] = y_c
+            e[2,:] = z_c - z_hat
+            
+            d_min  = np.linalg.norm( e , axis = 0 )
+            
+            ey     = e[1,:]
+            ez     = e[2,:]
+            
+            de_dx0  = np.sinh( x_c ) * np.cos( phi )
+            de_dy0  = np.sinh( x_c ) * np.sin( phi )
+            de_dphi = - np.sinh( x_c ) * ( - np.sin( phi ) * ( x_w - x0 ) + 
+                                             np.cos( phi ) * ( y_w - y0 ) ) 
+            
+            c = np.cos( phi )
+            s = np.sin( phi )
+            
+            # Error Grad for each pts
+            eT_de_dp = np.zeros( (5 , pts.shape[1] ) )
+            
+            eT_de_dp[0,:] = ey * s + ez * de_dx0
+            eT_de_dp[1,:] = ey * s + ez * de_dy0 
+            eT_de_dp[2,:] = -ez
+            eT_de_dp[3,:] = ey * ( c * ( x0 - x_w ) + s * ( y0 - y_w ) ) + ez * de_dphi
+            eT_de_dp[4,:] = -ez * ( np.cosh( x_c / a ) - x_c / a * np.sinh( x_c / a ) - 1 )
+            
+            # Norm grad
+            dd_dp = eT_de_dp / d_min
+        
+        ################################
         
         # Smoothing grad
         dc_dd = b * power * ( b * d_min ) ** ( power - 1 ) / ( np.log( 10 ) * ( l +  b * d_min ) ** power )
@@ -346,19 +423,6 @@ def cost_local_yz( p_hat , pts ):
     
     return costs.sum()
 
-############################
-def cost_dmin_sample( p_hat , pts , l = 1.0 ):
-    """ """
-    
-    d_min = d_min_sample( p_hat , pts )
-    
-    # Cost shaping function
-    pts_cost = lorentzian( d_min , l )
-    
-    # Sum of all costs
-    cost = pts_cost.sum()
-    
-    return cost
 
 
 # ###########################
@@ -383,7 +447,7 @@ def get_catanery_group( p_hat , pts , d_th = 1.0 , n_sample = 1000 , x_min = -20
 
 
 ###############################################################################
-class CateneryEstimationPlot:
+class catenaryEstimationPlot:
     """ 
     """
     
@@ -509,7 +573,7 @@ def noisy_p2r_w( p , x_min = -200, x_max = 200 , n = 400, w = 0.5 ):
 ############################
 def multiples_noisy_p2r_w( p , x_min, x_max , n , w ):
     """
-    Create 3D world pts for a list of catenery parameters vector
+    Create 3D world pts for a list of catenary parameters vector
 
     """
     

catenary/catenary_tests.py
@@ -9,7 +9,7 @@ import numpy as np
 import time
 from scipy.optimize import minimize
 
-import catenery
+import catenary
 
 
 ###########################
@@ -18,15 +18,15 @@ import catenery
 
 def cat_test():
     
-    t1 = ( catenery.cat( 0 , 1 )   == 0.0 )
-    t2 = ( catenery.cat( 1.0 , 1 ) == 0.5430806348152437 )
+    t1 = ( catenary.cat( 0 , 1 )   == 0.0 )
+    t2 = ( catenary.cat( 1.0 , 1 ) == 0.5430806348152437 )
     
     return (t1 & t2)
 
 
 def w_T_c_test():
     
-    T = catenery.w_T_c( 0 , 0 , 0 , 0 )
+    T = catenary.w_T_c( 0 , 0 , 0 , 0 )
     
     return T.shape == (4,4)
 
@@ -35,7 +35,7 @@ def p2r_w_test():
     
     p = np.array([0,0,0,0,1])
     
-    r_w, x_c = catenery.p2r_w( p , -50 , 50 , 101 )
+    r_w, x_c = catenary.p2r_w( p , -50 , 50 , 101 )
     
     return (r_w.shape == (3,101) ) & ( x_c.shape == (101,) )
 
@@ -55,7 +55,7 @@ def noisy_p2r_w_test():
     
     p = np.array([0,0,0,0,1])
     
-    r = catenery.noisy_p2r_w( p , -50 , 50 , 101 , 0.5  )
+    r = catenary.noisy_p2r_w( p , -50 , 50 , 101 , 0.5  )
     
     return (r.shape == (3,101) ) 
 
@@ -64,52 +64,54 @@ def multiples_noisy_p2r_w_test():
     
     p = np.array([ [0,0,0,0,1] , [0,0,0,0,2] ] ).T
     
-    r = catenery.multiples_noisy_p2r_w( p , [-10, -5], [10,-5] , [30,60] , [ 0.5 , 0.0 ] )
+    r = catenary.multiples_noisy_p2r_w( p , [-10, -5], [10,-5] , [30,60] , [ 0.5 , 0.0 ] )
     
     return (r.shape == (3,90) ) 
 
 
 def outliers_test():
     
-    r = catenery.outliers( 10 , [1,2,3] , 50. )
+    r = catenary.outliers( 10 , [1,2,3] , 50. )
     
     return (r.shape == (3,10) ) 
 
 
 def outliers_test():
     
-    r = catenery.outliers( 10 , [1,2,3] , 50. )
+    r = catenary.outliers( 10 , [1,2,3] , 50. )
     
     return (r.shape == (3,10) ) 
 
 
-def gradient_test():
+def gradient_test( method = 'sample' ):
     
     p_true  =  np.array([ 50.0,  50.0, 50.0, 1.0, 600.0])
     p       =  np.array([  0.0,   0.0,  0.0, 2.0, 800.0])
     
-    pts = catenery.p2r_w( p_true , 50 )[0]
+    pts = catenary.p2r_w( p_true , 50 )[0]
     
-    # plot = CateneryEstimationPlot( p_true , p , pts)
+    # plot = catenaryEstimationPlot( p_true , p , pts)
     
     # pts  = generate_test_data( p_true , n_obs = 20 , x_min = -30,
     #                                    x_max = 30, w_l = 0.5, n_out = 10, 
     #                                    center = [50,50,50] , w_o = 100 )
     
-    params = [ 'sample' , np.diag([ 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ]) ,
+    params = [ method, np.diag([ 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ]) ,
                             1.0 , 1.0 , 2 , 1000 , -200 , 200] 
     
     p_nom = p
     
-    grad_analytical = catenery.dJ_dp( p , pts, p_nom, params , False )
-    grad_numerical  = catenery.dJ_dp( p , pts, p_nom, params , True  )
+    grad_analytical = catenary.dJ_dp( p , pts, p_nom, params , False )
+    grad_numerical  = catenary.dJ_dp( p , pts, p_nom, params , True  )
+    
+    print( 'Analytical grad: ' , grad_analytical )
+    print( 'Numerical  grad: ' , grad_numerical )
     
     e = np.linalg.norm( grad_analytical - grad_numerical ) / np.linalg.norm( grad_numerical )
     
     succes = ( e < 0.1 )
     
     return succes
-
 
 
 
@@ -124,11 +126,11 @@ def plot_test():
     p_true  =  np.array([ 50 , 50 , 50 , 0.2 , 500 ])
     p_hat   =  np.array([  0 ,  0 ,  0 , 0   , 1000 ])
     
-    pts   = catenery.generate_test_data( p_true , n_obs = 20 , x_min = -100, 
+    pts   = catenary.generate_test_data( p_true , n_obs = 20 , x_min = -100, 
                                         x_max = 100, w_l = 0.5, n_out = 10, 
                                         center = [0,0,0] , w_o = 100 )
 
-    plot  = catenery.CateneryEstimationPlot(  p_true , p_hat , pts )
+    plot  = catenary.catenaryEstimationPlot(  p_true , p_hat , pts )
     
     return True
     
@@ -144,16 +146,16 @@ def animation_test():
     
     t = np.linspace( 0 , 10, 101 )
     
-    ( pts , p ) = catenery.generate_test_data_sequence( 0, p0 , dp_dt , partial_obs = True, 
+    ( pts , p ) = catenary.generate_test_data_sequence( 0, p0 , dp_dt , partial_obs = True, 
                                      n_obs = 20 , x_min = -100, x_max = 100, 
                                      w_l = 0.5, n_out = 10, center = [0,0,0] , 
                                      w_o = 100 )
 
-    plot  = catenery.CateneryEstimationPlot(  p , p_hat , pts )
+    plot  = catenary.catenaryEstimationPlot(  p , p_hat , pts )
     
     for i in range(101):
         
-        ( pts , p )  = catenery.generate_test_data_sequence( t[i], p0 , dp_dt , 
+        ( pts , p )  = catenary.generate_test_data_sequence( t[i], p0 , dp_dt , 
                                                     partial_obs = True, 
                                          n_obs = 20 , x_min = -100, x_max = 100, 
                                          w_l = 0.5, n_out = 10, center = [0,0,0] , 
@@ -177,11 +179,11 @@ def convergence_basic_test( method = 'sample' ,  grad = False ):
     p_true  =  np.array([ 32.0, 43.0, 77.0, 1.3, 53.0])
     p_init  =  np.array([ 10.0, 10.0, 10.0, 2.0, 80.0])
     
-    pts  = catenery.generate_test_data( p_true , n_obs = 20 , x_min = -30,
+    pts  = catenary.generate_test_data( p_true , n_obs = 20 , x_min = -30,
                                        x_max = 30, w_l = 0.5, n_out = 10, 
                                        center = [50,50,50] , w_o = 100 )
     
-    plot  = catenery.CateneryEstimationPlot(  p_true , p_init , pts , 50 , -50 , +50 )
+    plot  = catenary.catenaryEstimationPlot(  p_true , p_init , pts , 50 , -50 , +50 )
     
     bounds = [ (0,100), (0,100) , (0,100) , (0,3.14) , (10,200) ]
     
@@ -190,10 +192,10 @@ def convergence_basic_test( method = 'sample' ,  grad = False ):
     
     start_time = time.time()
     
-    func = lambda p: catenery.J(p, pts, p_init, params)
+    func = lambda p: catenary.J(p, pts, p_init, params)
     
     if grad:
-        jac = lambda p: catenery.dJ_dp( p, pts, p_init, params)
+        jac = lambda p: catenary.dJ_dp( p, pts, p_init, params)
     else:
         jac = None
     
@@ -224,16 +226,16 @@ def tracking_basic_test( method = 'sample' ,  grad = False , partial_obs = False
     
     t = np.linspace( 0 , 5, 51 )
     
-    ( pts , p ) = catenery.generate_test_data_sequence( 0, p0 , dp_dt , partial_obs, 
+    ( pts , p ) = catenary.generate_test_data_sequence( 0, p0 , dp_dt , partial_obs, 
                                      n_obs = 20 , x_min = -30, x_max = 30, 
                                      w_l = 0.5, n_out = 10, center = [50,50,50] , 
                                      w_o = 100 )
     
-    plot  = catenery.CateneryEstimationPlot( p , p_hat , pts , 50 , -50 , +50 )
+    plot  = catenary.catenaryEstimationPlot( p , p_hat , pts , 50 , -50 , +50 )
     
     for i in range(51):
         
-        ( pts , p )  = catenery.generate_test_data_sequence( t[i], p0 , dp_dt , 
+        ( pts , p )  = catenary.generate_test_data_sequence( t[i], p0 , dp_dt , 
                                                     partial_obs, 
                                          n_obs = 20 , x_min = -30, x_max = 30, 
                                          w_l = 0.5, n_out = 10, center = [50,50,50] , 
@@ -247,10 +249,10 @@ def tracking_basic_test( method = 'sample' ,  grad = False , partial_obs = False
         
         start_time = time.time()
         
-        func = lambda p: catenery.J(p, pts, p_hat, params)
+        func = lambda p: catenary.J(p, pts, p_hat, params)
         
         if grad:
-            jac = lambda p: catenery.dJ_dp( p, pts, p_hat, params)
+            jac = lambda p: catenary.dJ_dp( p, pts, p_hat, params)
         else:
             jac = None
         
@@ -283,16 +285,16 @@ def tracking_advanced_test( method = 'sample' ,  grad = False , partial_obs = Fa
     
     t = np.linspace( 0 , 20, 201 )
     
-    ( pts , p ) = catenery.generate_test_data_sequence( 0, p0 , dp_dt , partial_obs = True, 
+    ( pts , p ) = catenary.generate_test_data_sequence( 0, p0 , dp_dt , partial_obs = True, 
                                      n_obs = 20 , x_min = -100, x_max = 100, 
                                      w_l = 0.5, n_out = 10, center = [0,0,0] , 
                                      w_o = 100 )
 
-    plot  = catenery.CateneryEstimationPlot(  p , p_hat , pts )
+    plot  = catenary.catenaryEstimationPlot(  p , p_hat , pts )
     
     for i in range(201):
         
-        ( pts , p )  = catenery.generate_test_data_sequence( t[i], p0 , dp_dt , 
+        ( pts , p )  = catenary.generate_test_data_sequence( t[i], p0 , dp_dt , 
                                                     partial_obs = True, 
                                          n_obs = 20 , x_min = -100, x_max = 100, 
                                          w_l = 0.5, n_out = 10, center = [0,0,0] , 
@@ -309,8 +311,8 @@ def tracking_advanced_test( method = 'sample' ,  grad = False , partial_obs = Fa
         
         start_time = time.time()
         
-        func = lambda p: catenery.J(p, pts, p_hat, params)
-        grad = lambda p: catenery.dJ_dp( p, pts, p_hat, params)
+        func = lambda p: catenary.J(p, pts, p_hat, params)
+        grad = lambda p: catenary.dJ_dp( p, pts, p_hat, params)
         
         res = minimize( func,
                         p_hat, 
@@ -342,11 +344,11 @@ def grouping_test():
     p_true  =  np.array([ 32.0, 43.0, 77.0 , 1.3, 530.])
     p_init  =  np.array([ 10.0, 10.0, 10.0 , 2.0, 800.])
     
-    pts  = pts  = catenery.generate_test_data( p_true , n_obs = 20 , x_min = -30,
+    pts  = pts  = catenary.generate_test_data( p_true , n_obs = 20 , x_min = -30,
                                        x_max = 30, w_l = 0.5, n_out = 10, 
                                        center = [50,50,50] , w_o = 100 )
     
-    plot  = catenery.CateneryEstimationPlot( p_true , p_init , pts , 50 , -50 , +50 )
+    plot  = catenary.catenaryEstimationPlot( p_true , p_init , pts , 50 , -50 , +50 )
     
     # ###########################
     # # Optimization
@@ -358,8 +360,8 @@ def grouping_test():
     
     start_time = time.time()
     
-    func = lambda p: catenery.J(p, pts, p_init, params)
-    grad = lambda p: catenery.dJ_dp( p, pts, p_init, params)
+    func = lambda p: catenary.J(p, pts, p_init, params)
+    grad = lambda p: catenary.dJ_dp( p, pts, p_init, params)
     
     res = minimize( func, 
                     p_init, 
@@ -377,7 +379,7 @@ def grouping_test():
            f" True: {np.array2string(p_true, precision=2, floatmode='fixed')} \n" + 
            f" Hat : {np.array2string(p_hat, precision=2, floatmode='fixed')}  \n" )
     
-    pts_in = catenery.get_catanery_group( p_hat , pts , 2.0 )
+    pts_in = catenary.get_catanery_group( p_hat , pts , 2.0 )
     
     plot.ax.plot( pts_in[0,:] , pts_in[1,:] , pts_in[2,:], 'o' , label= 'Group')
 
@@ -386,24 +388,27 @@ def grouping_test():
 if __name__ == "__main__":     
     """ MAIN TEST """
     
-    print('catenery.cat: ', cat_test())
-    print('catenery.w_T_c: ', w_T_c_test())
-    print('catenery.p2r_w: ', p2r_w_test())
-    print('catenery.noisy_p2r_w: ', noisy_p2r_w_test())
-    print('catenery.multiples_noisy_p2r_w: ', multiples_noisy_p2r_w_test())
-    print('catenery.outliers: ', outliers_test())
-    print('catenery.dJ_dp: ', gradient_test())
+    print('catenary.cat: ', cat_test())
+    print('catenary.w_T_c: ', w_T_c_test())
+    print('catenary.p2r_w: ', p2r_w_test())
+    print('catenary.noisy_p2r_w: ', noisy_p2r_w_test())
+    print('catenary.multiples_noisy_p2r_w: ', multiples_noisy_p2r_w_test())
+    print('catenary.outliers: ', outliers_test())
+    print('catenary.dJ_dp (samples) : ', gradient_test())
+    print('catenary.dJ_dp (x) : ', gradient_test( 'x' ) )
     
     
     # plot_test()
     # animation_test()
     
     # convergence_basic_test()
+    # convergence_basic_test( method = 'x' )
     # convergence_basic_test( grad = True )
+    # convergence_basic_test( method = 'x' , grad = True )
     
     # tracking_basic_test()
     # tracking_basic_test( grad = True )
     
     # tracking_advanced_test()
     
-    grouping_test()
+    # grouping_test()